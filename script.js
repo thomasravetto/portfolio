947ec837373e8ee346085cd9de9585fdbba8ca62
ThomasRavetto <travetto18@gmail.com>
2024-03-12 20:06:38 +0100

    updating website

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-const daysElement = document.getElementById("days");
-const hoursElement = document.getElementById('hours');
-const minutesElement = document.getElementById('minutes');
-const secondsElement = document.getElementById('seconds');
+/**Slideshow images functions */
 
-const countDownDate = new Date('03/04/2024 00:00:00');
+let slideIndex = 1;
+showSlides(slideIndex);
 
-const x = setInterval(() => {
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-    let now = new Date().getTime();
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-    let distance = countDownDate - now;
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("project_slider_element");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
 
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    daysElement.innerText = days;
-    hoursElement.innerText = hours;
-    minutesElement.innerText = minutes;
-    secondsElement.innerText = seconds;
-
-}, 1000);
+  slides[slideIndex - 1].style.display = "block";
+}
